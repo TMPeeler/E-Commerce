106c48589a2685ce9a64b0aa919dc8071a71897e
Thomas Peeler <Thomasmwpeeler@Gmail.com>
2021-04-28 17:25:20 -0700

    begin to outline relationships and change env details

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,10 +5,27 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+
+
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+    foreignKey: 'product_id',
+    //not sure if this is correct
+
+});
 
 // Products belongToMany Tags (through ProductTag)
+
+Product.belongsToMany(Tag, {
+    through: {
+        model: 'ProductTag',
+    }
+    //as: '____',
+
+});
 
 // Tags belongToMany Products (through ProductTag)
 
